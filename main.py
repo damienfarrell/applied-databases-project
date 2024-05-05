abf9b4ee7e526a37df5ffe486d75b2a4a929588a
damienfarrell <damien.farrell1@gmail.com>
2024-05-05 21:38:25 +0100

    dynamically import modules

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from setup import install_packages, required_packages
-from menu import menu, view_city_by_country, update_city_population, add_new_person, delete_person, view_countries_by_population, show_twinned_cities, twin_with_dublin
 import time
-from yaspin import yaspin
+import importlib
 
 def main():
-    
     install_packages(required_packages)
 
+    menu_module = importlib.import_module("menu")
+    yaspin_module = importlib.import_module("yaspin")
+    
     choice_to_function = {
-        '1': view_city_by_country,
-        '2': update_city_population,
-        '3': add_new_person,
-        '4': delete_person,
-        '5': view_countries_by_population,
-        '6': show_twinned_cities,
-        '7': twin_with_dublin,
+        '1': menu_module.view_city_by_country,
+        '2': menu_module.update_city_population,
+        '3': menu_module.add_new_person,
+        '4': menu_module.delete_person,
+        '5': menu_module.view_countries_by_population,
+        '6': menu_module.show_twinned_cities,
+        '7': menu_module.twin_with_dublin,
         'x': lambda: print("Exiting the application...")
     }
+
     while True:
-        with yaspin(text="Loading main menu...", color="blue"):        
+        with yaspin_module.yaspin(text="Loading main menu...", color="blue"):
             time.sleep(1)
-        choice = menu()
+        choice = menu_module.menu()
         if choice == 'x':
             break
         if choice in choice_to_function:
